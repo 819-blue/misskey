6f5f884b85ce7aeb19aa9aeb37d7c606787db6f8
kyori19 <kyori.dis@gmail.com>
2018-11-25 11:26:27 +0900

    リアクション7と8の位置を入れ替え

--- src/client/app/common/views/components/reaction-picker.vue
+++ src/client/app/common/views/components/reaction-picker.vue
@@ -10,8 +10,8 @@
 			<button @click="react('hmm')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="4" :title="$t('@.reactions.hmm')"><mk-reaction-icon reaction='hmm'/></button>
 			<button @click="react('surprise')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="5" :title="$t('@.reactions.surprise')"><mk-reaction-icon reaction='surprise'/></button>
 			<button @click="react('congrats')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="6" :title="$t('@.reactions.congrats')"><mk-reaction-icon reaction='congrats'/></button>
-			<button @click="react('angry')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="7" :title="$t('@.reactions.angry')"><mk-reaction-icon reaction='angry'/></button>
-			<button @click="react('confused')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="8" :title="$t('@.reactions.confused')"><mk-reaction-icon reaction='confused'/></button>
+			<button @click="react('confused')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="7" :title="$t('@.reactions.confused')"><mk-reaction-icon reaction='confused'/></button>
+			<button @click="react('angry')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="8" :title="$t('@.reactions.angry')"><mk-reaction-icon reaction='angry'/></button>
 			<button @click="react('rip')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="9" :title="$t('@.reactions.rip')"><mk-reaction-icon reaction='rip'/></button>
 			<button @click="react('pudding')" @mouseover="onMouseover" @mouseout="onMouseout" tabindex="10" :title="$t('@.reactions.pudding')"><mk-reaction-icon reaction='pudding'/></button>
 		</div>
@@ -87,8 +87,8 @@ export default Vue.extend({
 				'4': () => this.react('hmm'),
 				'5': () => this.react('surprise'),
 				'6': () => this.react('congrats'),
-				'7': () => this.react('angry'),
-				'8': () => this.react('confused'),
+				'7': () => this.react('confused'),
+				'8': () => this.react('angry'),
 				'9': () => this.react('rip'),
 				'0': () => this.react('pudding'),
 			};
